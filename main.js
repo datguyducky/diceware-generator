9c59e951bfca41f5e16ab3e7e9620a0fdca3222f
datguysheepy <t.mysl@protonmail.com>
2019-07-02 17:54:35 +0200

    better comments

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 function diceWare() {
     //generating passphrase part
-    var numPhrase = document.getElementById('num-phrase').value; // how many words user wants to generate
+    var numPhrase = document.getElementById('num-phrase').value; // how many words user wants to have in passphrase
     var language = document.getElementById('languages').value; // language for word list 
-    var what = "";
+    var what = ""; // this is placeholder for fully generated passphrase, this is what we return to user 
 
     if (numPhrase < 6 || numPhrase > 128) {
-        document.getElementById("error").style.display = "block";
+        document.getElementById("error").style.display = "block"; //displaying error if user typed less than 6 or more than 128
     } else {
         document.getElementById("error").style.display = "none";
-        for (i = 1; i <= numPhrase; i++) { // loop for generating random set of number (from 1 to 6). Length of loop depends on user input.
-            var cont = "";
+        for (i = 1; i <= numPhrase; i++) { // loop for generating random set of number (from 1 to 6). Length of loop depends on user input
+            var cont = ""; //placeholder for set of numbers than we generated (ex. 11111) - this is index number for language word-list array
 
             for (j = 1; j <= 5; j++) {
 
-                var result = Math.floor(Math.random() * 6) + 1; // generating 5 times random number (from 1 to 6) which will get us something like for ex. 11111. Then we check which language user selected and use appropiate file, which contains list of words.
+                var result = Math.floor(Math.random() * 6) + 1; // generating 5 times random number (from 1 to 6) which will get us something like for ex. 11111
                 result.toString;
-                cont += result;
+                cont += result; //storing result in 'cont' variable
             }
 
             if (language == "zh") { //using chinese word list
@@ -79,24 +79,10 @@ function copyText() {
     //copying text to clipboard part
     var text = document.getElementById("gen-result-p").innerText; //element to copy text from
 
-    var elem = document.createElement("textarea");
+    var elem = document.createElement("textarea"); //creating textarea so we can use 'elem.select()' when we really have text in '<p>' tag
     document.body.appendChild(elem);
     elem.value = text;
     elem.select();
     document.execCommand("copy");
-    document.body.removeChild(elem);
-
-    console.log(elem.value);
+    document.body.removeChild(elem); //deleting textarea
 }
-
-function tooltipText(a) {
-    var tooltip = document.getElementById("my-tooltip");
-    var tooltip2 = document.getElementById("my-tooltip2");
-   
-    if(a == 'gen'){
-        tooltip.innerHTML = "Generate passphrase";
-    }
-    else{
-        tooltip2.innerHTML = "Copy to clipboard";
-    }
-  }
